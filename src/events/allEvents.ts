82715946b214a57511c72a2d868334b17231634b
Reliodas <34870740+Reliodas@users.noreply.github.com>
2022-11-06 21:58:35 +0100

    [#2] Add Ping Command (#10)
    Closes [#2]

--- src/events/allEvents.ts
+++ src/events/allEvents.ts
@@ -1,3 +1,7 @@
+import { interactionCreate } from './interactionCreate';
 import { ready } from './ready';
 
-export const allEvents = [ready];
+export const allEvents = [
+    ready, 
+    interactionCreate
+];
--- src/types/ExtendedClient.ts
+++ src/types/ExtendedClient.ts
@@ -2,11 +2,11 @@ import { Client, ClientOptions, Collection } from 'discord.js';
 import { SlashCommand } from './SlashCommand';
 
 export class ExtendedClient extends Client {
-	commands: Collection<string, SlashCommand>;
+	slashCommands: Collection<string, SlashCommand>;
 
 	constructor(options: ClientOptions) {
 		super(options);
-		this.commands = new Collection();
+		this.slashCommands = new Collection();
 	}
 
 }
